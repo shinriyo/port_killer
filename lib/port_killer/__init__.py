6185bf924b4b52e85138049bb0f6a8cc64e44923
shinriyo <shinriyo@gmail.com>
2016-09-24 14:26:10 +0900

    mistook
    fix desctiption

--- lib/port_killer/__init__.py
+++ lib/port_killer/__init__.py
@@ -1,11 +1,11 @@
 # package information.
 INFO = dict(
-    name="Mew Calc",
-    description="The calculator fot cat's age",
+    name="port killer",
+    description="The killing port tool",
     author="shinriyo",
     author_email="shinriyo@gmail.com",
     license="MIT License",
-    url="https://github.com/shinriyo/cat_age_calculator",
+    url="https://github.com/shinriyo/port_killer",
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License"
